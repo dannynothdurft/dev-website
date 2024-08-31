3687ce46027c6265b834059fd0b4d436f110c968
danny <danny.nothdurft@icloud.com>
2024-08-31 11:46:05 +0200

    kleine anpassung

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
     <main>
       <Hero />
       <section className="container flex col center my-1">
-        <h2 className="h2">{HomeLang.company.content}</h2>
+        <h2>{HomeLang.company.content}</h2>
         <div className="flex row">
           {HomeLang.company.companys.map((com) => {
             return <ICONLINK href={com.href} svg={com.src} title={com.title} />;
           })}
         </div>
       </section>
-      <hr className="hr" />
+      <hr />
     </main>
   );
 }
